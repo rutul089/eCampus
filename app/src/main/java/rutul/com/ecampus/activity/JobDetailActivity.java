08fde5fd68c67c7d74786818a6bf306d661e6b3f
rutul.mehta <rutul089@gmail.com>
2018-11-12 18:45:27 +0530

    -Job Detail Activity

---
+++ app/src/main/java/rutul/com/ecampus/activity/JobDetailActivity.java
@@ -0,0 +1,29 @@
+package rutul.com.ecampus.activity;
+
+import android.os.Bundle;
+
+import rutul.com.ecampus.R;
+
+public class JobDetailActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_job_detail);
+    }
+
+    @Override
+    public void initComponents() {
+
+    }
+
+    @Override
+    public void setListener() {
+
+    }
+
+    @Override
+    public void prepareViews() {
+
+    }
+}
